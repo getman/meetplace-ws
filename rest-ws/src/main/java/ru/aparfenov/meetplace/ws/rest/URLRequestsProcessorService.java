64611bf81b05519450dcbf21463dcc3d83239200
AParfenov <getmanartem@gmail.com>
2017-10-16 20:04:53 +0300

    Created a blank JAX-RS web service using Jersey

---
+++ rest-ws/src/main/java/ru/aparfenov/meetplace/ws/rest/URLRequestsProcessorService.java
@@ -0,0 +1,24 @@
+package ru.aparfenov.meetplace.ws.rest;
+
+/**
+ * Created by AParfenov on 16.10.2017.
+ */
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class URLRequestsProcessorService {
+
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Jersey say : " + msg;
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+}
